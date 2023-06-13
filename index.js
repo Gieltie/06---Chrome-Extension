47980192d225d6ae5f159af1531ecfc92de1a8d6
Gael Hofman <Gieltie@outlook.fr>
2023-06-13 15:27:56 +0200

    added a delete button to clear out local storage and my leads and the dom

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-// ["lead1", "lead2"] or null
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 console.log(leadsFromLocalStorage)
-// 1. Check if leadsFromLocalStorage is truthy
-// 2. If so, set myLeads to its value and call renderLeads()
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
@@ -18,6 +16,13 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    console.log("double clicked!")
+    localStorage.clear()
+    myLeads = []
     renderLeads()
 })
 
